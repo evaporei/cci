320f5ef782d6892775b392b70d3eba84348cb6c2
Eva Pace <eba.pachi@gmail.com>
2023-06-19 16:08:00 -0300

    arrays-and-strings: permutation w/ char count

--- 01-arrays-and-strings/rust/src/lib.rs
+++ 01-arrays-and-strings/rust/src/lib.rs
@@ -73,10 +73,46 @@ pub fn check_permutation(a: &str, b: &str) -> bool {
     sorted_a == sorted_b // O(n) -> doesn't count
 }
 
+use std::collections::HashMap;
+
+// a = b = n
+// O(n)
+pub fn check_permutation_char_count(a: &str, b: &str) -> bool {
+    if a.len() != b.len() { // O(1)
+        return false;
+    }
+
+    let mut letters = HashMap::new();
+
+    for ch in a.chars() { // O(a)
+        letters.entry(ch) // O(1) amortized
+            .and_modify(|count| *count += 1) // O(1) amortized
+            .or_insert(1); // O(1) amortized
+    }
+
+
+    for ch in b.chars() { // O(b)
+        letters.entry(ch) // O(1) amortized
+            .and_modify(|count| *count -= 1) // O(1) amortized
+            .or_insert(-1); // O(1) amortized
+
+        if *letters.get(&ch).unwrap() < 0 { // O(1) amortized
+            return false;
+        }
+    }
+
+    true
+}
+
 #[test]
 fn test_check_permutation() {
     assert!(check_permutation("abc", "cab"));
     assert!(!check_permutation("abc", "zef"));
     assert!(!check_permutation("abc", "abj"));
     assert!(!check_permutation("abcd", "abc"));
+
+    assert!(check_permutation_char_count("abc", "cab"));
+    assert!(!check_permutation_char_count("abc", "zef"));
+    assert!(!check_permutation_char_count("abc", "abj"));
+    assert!(!check_permutation_char_count("abcd", "abc"));
 }
