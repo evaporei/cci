63d7abf94821c07869ce2765b7fff900db9b685e
Eva Pace <eba.pachi@gmail.com>
2023-06-24 21:52:36 -0300

    linked-lists: add remove dup from unordered list (no rust yet)

--- 02-linked-lists/javascript/index.js
+++ 02-linked-lists/javascript/index.js
@@ -37,3 +37,44 @@ var deleteDupsOrdered = function(head) {
     new ListNode(1, new ListNode(2, new ListNode(3)))
   );
 })();
+
+// 2.1 Remove Dups! Write code to remove duplicates from an unsorted linked list.
+// FOLLOW UP
+// How would you solve this problem if a temporary buffer is not allowed?
+// Hints: #9, #40
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDupsUnordered = function(head) {
+  const set = new Set();
+  let node = head;
+  let prev;
+
+  while (node != null) {
+    if (set.has(node.val)) {
+      prev.next = node.next;
+    } else {
+      set.add(node.val);
+      prev = node;
+    }
+
+    node = node.next;
+  }
+
+  return head;
+};
+
+(function testDelDupOrd() {
+  const l1 = new ListNode(2, new ListNode(1, new ListNode(2)));
+  assert.deepStrictEqual(
+    deleteDupsUnordered(l1),
+    new ListNode(2, new ListNode(1))
+  );
+
+  const l2 = new ListNode(3, new ListNode(2, new ListNode(3, new ListNode(1, new ListNode(2)))));
+  assert.deepStrictEqual(
+    deleteDupsUnordered(l2),
+    new ListNode(3, new ListNode(2, new ListNode(1)))
+  );
+})();
