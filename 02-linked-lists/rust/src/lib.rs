ba917fab47aba8845985b19b938be5bbfcacc461
Eva Pace <eba.pachi@gmail.com>
2023-06-25 14:24:25 -0300

    linked-lists: fix indentation of ListNode

--- 02-linked-lists/rust/src/lib.rs
+++ 02-linked-lists/rust/src/lib.rs
@@ -1,16 +1,16 @@
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
-  pub val: i32,
-  pub next: Option<Box<ListNode>>
+    pub val: i32,
+    pub next: Option<Box<ListNode>>
 }
 
 impl ListNode {
-  pub fn new(val: i32) -> Self {
-    ListNode {
-      next: None,
-      val
+    pub fn new(val: i32) -> Self {
+        ListNode {
+            next: None,
+            val
+        }
     }
-  }
 }
 
 impl FromIterator<i32> for ListNode {
