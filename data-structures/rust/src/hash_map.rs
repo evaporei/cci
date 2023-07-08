22ce30e8d11b723f375ae5901c0e92517daddd21
Eva Pace <eba.pachi@gmail.com>
2023-07-08 09:23:08 -0300

    data-structures: base hash map, no linked list yet

---
+++ data-structures/rust/src/hash_map.rs
@@ -0,0 +1,46 @@
+fn adler32(text: &str) -> u32 {
+  let mut s1 = 1;
+  let mut s2 = 0;
+
+  for c in text.chars() {
+    let n = u32::from(c);
+    s1 = (s1 + n) % 65521;
+    s2 = (s2 + s1) % 65521;
+  }
+
+  return (s2 << 16) | s1;
+}
+
+const BASE_CAP: usize = 500;
+
+pub struct HashMap<V> {
+    entries: [Option<V>; BASE_CAP],
+}
+
+impl<V: Copy> HashMap<V> {
+    pub fn new() -> Self {
+        Self { entries: [None; BASE_CAP] }
+    }
+
+    pub fn get(&self, key: &str) -> Option<&V> {
+        let bucket = adler32(key) as usize % BASE_CAP;
+        self.entries[bucket].as_ref()
+    }
+
+    pub fn set(&mut self, key: &str, value: V) {
+        let bucket = adler32(key) as usize % BASE_CAP;
+        self.entries[bucket] = Some(value);
+    }
+}
+
+#[test]
+fn test_map() {
+    let mut map = HashMap::new();
+    assert_eq!(map.get("2"), None);
+
+    map.set("2", 50);
+    assert_eq!(map.get("2"), Some(&50));
+
+    map.set("2", 60);
+    assert_eq!(map.get("2"), Some(&60));
+}
