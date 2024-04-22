c93f5758d39ac9a6c1843576fcfc61e9b686b77e
Eva Pace <eba.pachi@gmail.com>
2024-04-22 11:33:40 -0300

    array-and-strings: string compression python leetcode version

--- 01-arrays-and-strings/python/06-str-compression.py
+++ 01-arrays-and-strings/python/06-str-compression.py
@@ -31,32 +31,45 @@ assert compress("abcdefg") == "abcdefg"
 
 
 # b -> b
-def leet(s: str) -> str:
-    out = []
-    letter = s[0]
-    count = 0
-    for ch in s:
-        if ch == letter:
-            count += 1
-        else:
-            out.append(letter)
-            if count != 1:
-                out.append(str(count))
-            letter = ch
-            count = 1
+def leet(s: list[str]) -> int:
+    old_len = len(s)
 
-    out.append(letter)
-    if count != 1:
-        out.append(str(count))
+    index = 0
+    i = 0
+    while i < len(s):
+        j = i
+        while j < len(s) and s[i] == s[j]:
+            j += 1
+        s[index] = s[i]
+        index += 1
+        count = j - i
+        if count > 1:
+            for ch in str(count):
+                s[index] = ch
+                index += 1
+        i = j
 
-    if len(out) >= len(s):
-        return s
-    return "".join(out)
+    if old_len > index:
+        del s[index:]
+        return index
+
+    return old_len
 
 
-assert leet("aaaaabcccccdeeef") == "a5bc5de3f"
-assert leet("aaabbbcccddd") == "a3b3c3d3"
-assert leet("aabbbbcdddd") == "a2b4cd4"
-assert leet("aabcccccaaa") == "a2bc5a3"
-assert leet("abccddefg") == "abccddefg"
-assert leet("abcdefg") == "abcdefg"
+expected = {
+    "aaaaabcccccdeeef": "a5bc5de3f",
+    "aaabbbcccddd": "a3b3c3d3",
+    "aabbbbcdddd": "a2b4cd4",
+    "aabcccccaaa": "a2bc5a3",
+    "abccddefg": None,
+    "abcdefg": None
+}
+
+for input, output in expected.items():
+    l_input = list(input)
+    length = leet(l_input)
+    if output:
+        assert length == len(output)
+        assert l_input == list(output)
+    else:
+        assert length == len(input)
