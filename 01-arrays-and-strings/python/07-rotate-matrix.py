2c2e43618b9ef436ce53d1fd8d9431e70a779db7
Eva Pace <eba.pachi@gmail.com>
2024-05-03 19:54:10 -0300

    array-and-strings: rotate matrix python

---
+++ 01-arrays-and-strings/python/07-rotate-matrix.py
@@ -0,0 +1,24 @@
+# 90 degrees
+def rotate(m: list[list[int]]):
+    for first in range(int(len(m) / 2)):
+        last = len(m) - first - 1
+        for i in range(first, last):
+            offset = i - first
+            # save
+            top = m[first][i]
+            # left -> top
+            m[first][i] = m[last - offset][first]
+            # bottom -> left
+            m[last - offset][first] = m[last][last - offset]
+            # right -> bottom
+            m[last][last - offset] = m[i][last]
+            # top -> right
+            m[i][last] = top
+
+
+# 1 2 3      7 4 1
+# 4 5 6  ->  8 5 2
+# 7 8 9      9 6 3
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+rotate(matrix)
+assert matrix == [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
