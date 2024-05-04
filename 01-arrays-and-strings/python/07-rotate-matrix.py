83d3ca2e8904d3b6f2a78e89098440bef6895579
Eva Pace <eba.pachi@gmail.com>
2024-05-04 18:16:34 -0300

    array-and-strings: rotate matrix neet code python

--- 01-arrays-and-strings/python/07-rotate-matrix.py
+++ 01-arrays-and-strings/python/07-rotate-matrix.py
@@ -16,9 +16,36 @@ def rotate(m: list[list[int]]):
             m[i][last] = top
 
 
+def neet(m: list[list[int]]):
+    left, right = 0, len(matrix) - 1
+
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+
+            # save
+            top_left = m[top][left + i]
+            
+            m[top][left + i] = m[bottom - i][left]
+
+            m[bottom - i][left] = m[bottom][right - i]
+            
+            m[bottom][right - i] = m[top + i][right]
+
+            m[top + i][right] = top_left
+
+        left += 1
+        right -= 1
+
+
+
 # 1 2 3      7 4 1
 # 4 5 6  ->  8 5 2
 # 7 8 9      9 6 3
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 rotate(matrix)
+assert matrix == [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+neet(matrix)
 assert matrix == [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
