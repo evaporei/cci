51310ace375121f9561def37ac6166cf0edfefb3
Eva Pace <eba.pachi@gmail.com>
2024-05-08 14:59:02 -0300

    linked-lists/python: add O(1) space solution remove duplicates

--- 02-linked-lists/python/01-remove-dups.py
+++ 02-linked-lists/python/01-remove-dups.py
@@ -1,6 +1,8 @@
 from linkedlist import LinkedList
 
-def remove_dups(ll: LinkedList[int]):
+# time: O(n)
+# space: O(m) m = unique items
+def remove_dups_fast(ll: LinkedList[int]):
     s = set()
     curr = ll.head
     prev = None
@@ -14,6 +16,29 @@ def remove_dups(ll: LinkedList[int]):
         curr = curr.next
 
 
+# time: O(nˆ2)
+# space: O(1)
+def remove_dups_small(ll: LinkedList[int]):
+    curr = ll.head
+
+    while curr is not None:
+        runner = curr
+        while runner.next:
+            if curr.value == runner.next.value:
+                runner.next = runner.next.next
+            else:
+                runner = runner.next
+        curr = curr.next
+
+
 linked = LinkedList.from_list([5, 5, 3, 6, 2, 6])
-remove_dups(linked)
+remove_dups_fast(linked)
+assert linked.to_list() == [5, 3, 6, 2], linked.to_list()
+
+linked = LinkedList.from_list([5, 5, 3, 6, 2, 6])
+remove_dups_small(linked)
+assert linked.to_list() == [5, 3, 6, 2], linked.to_list()
+
+linked = LinkedList.from_list([5, 3, 5, 6, 2, 6])
+remove_dups_small(linked)
 assert linked.to_list() == [5, 3, 6, 2], linked.to_list()
