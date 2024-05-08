6a9cfff82c946aedec3759402049de4adea1bd16
Eva Pace <eba.pachi@gmail.com>
2024-05-08 14:49:29 -0300

    linked-lists/python: simplify remove duplicates

--- 02-linked-lists/python/01-remove-dups.py
+++ 02-linked-lists/python/01-remove-dups.py
@@ -3,22 +3,14 @@ from linkedlist import LinkedList
 def remove_dups(ll: LinkedList[int]):
     s = set()
     curr = ll.head
-    prev = curr
+    prev = None
 
-    if curr is None:
-        return
-    
-    while curr.next is not None:
+    while curr is not None:
         if curr.value in s:
             prev.next = curr.next
-            curr = curr.next
         else:
             s.add(curr.value)
             prev = curr
-            curr = curr.next
-
-    if curr.value in s:
-        prev.next = curr.next
         curr = curr.next
 
 
